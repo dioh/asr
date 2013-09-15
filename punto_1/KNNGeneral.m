670ff96a929d64e3041a3567c7dfa17baa4c52f8
danito <danito@manzi.(none)>
2013-09-15 19:19:58 -0300

    punto 3

---
+++ punto_1/KNNGeneral.m
@@ -0,0 +1,46 @@
+function ind = KNNGeneral(k, testWave, trainPath )
+
+folderPath = trainPath;
+
+kDistances = inf(1,k);
+kNumbers = inf(1,k);
+
+trainingData.groups{1}= 2;
+%% trainingData.groups{2}= 1:3;
+trainingData.sex = ['m','f'];
+trainingData.repets = 3;
+trainingData.speed = ['n', 'f', 's'];
+
+super_array = [];
+index = 1;
+
+for nNum=0:10
+     for groupN=1:length(trainingData.groups)
+         for nSpeaker=1:length(trainingData.groups{groupN})
+             for ispeed=1:length(trainingData.speed);
+                 for nRep=1:trainingData.repets;
+                     trainFileName =  ['s',num2str(trainingData.groups{groupN}(nSpeaker)),trainingData.sex(groupN),...
+                     num2str(nRep),trainingData.speed(ispeed),'-',num2str(nNum)];
+                     waveformTrain = load([folderPath,trainFileName,'.mfcc']);
+                     D = dtw(waveformTrain, testWave,  0);
+                     [kDistances,kNumbers] = addToKArray(kDistances, kNumbers, D, nNum);
+               end
+           end
+       end
+   end
+end
+ind = mode(kNumbers);
+end
+
+function [kDistances,kNumbers] = addToKArray(kDistances, kNumbers, distance, number)
+    [maxDist, position] = max(kDistances);
+    if (distance >= maxDist)
+        return;
+    else
+        kDistances(position) = distance;
+        kNumbers(position) = number;
+    end
+    return;
+ 
+end
+ 
